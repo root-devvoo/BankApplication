c49a5fe9b659f857ef91e3434e3dd9c33e90e504
Woocheol Jung <jwcnet93@gmail.com>
2023-05-06 17:46:50 +0900

    [2023-05-06] successfulAuthentication 테스트 ~ SecurityConfig에 ROLE_prefix 관련 공식 문서 변경 확인하기까지 완료 (섹션 3 : 스프링부트 JWT 인증과 인가)

--- src/test/java/shop/mtcoding/bank/web/UserControllerTest.java
+++ src/test/java/shop/mtcoding/bank/web/UserControllerTest.java
@@ -3,6 +3,8 @@ package shop.mtcoding.bank.web;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import javax.transaction.Transactional;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +21,7 @@ import shop.mtcoding.bank.config.dummy.DummyObject;
 import shop.mtcoding.bank.domain.user.UserRepository;
 import shop.mtcoding.bank.dto.user.UserReqDto.JoinReqDto;
 
+@Transactional
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = WebEnvironment.MOCK)
 public class UserControllerTest extends DummyObject {
@@ -32,7 +35,7 @@ public class UserControllerTest extends DummyObject {
 
     @BeforeEach // BeforeEach는 모든 메서드가 실행되기 직전에 실행됨
     public void setUp() {
-        dataSetting();
+        userRepository.save(newUser("ssar", "쌀"));
     }
 
     @Test
@@ -78,9 +81,5 @@ public class UserControllerTest extends DummyObject {
 
         // then
         resultActions.andExpect(status().isBadRequest());
-    }
-
-    private void dataSetting() {
-        userRepository.save(newUser("ssar", "쌀"));
     }
 }
